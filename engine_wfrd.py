f2eebf7cc91f436d5c073e0658246372801e2318
ronykick24 <wert.pride@gmail.com>
2026-03-25 04:58:58 +0300

    Update engine_wfrd.py

--- engine_wfrd.py
+++ engine_wfrd.py
@@ -1,38 +1,32 @@
 import numpy as np
-from scipy.optimize import differential_evolution, least_squares
+from scipy.optimize import differential_evolution
 
 class WFRD_Engine_Core:
     def forward_model(self, m, md, inc, dip, n_layers):
-        res = m[:n_layers]
-        thick = m[n_layers:2*n_layers-1]
-        ani = m[-1]
+        res, thick, ani = m[:n_layers], m[n_layers:2*n_layers-1], m[-1]
         alpha_rel = np.radians(inc - dip)
         tvd_rel = md * np.sin(alpha_rel)
-        
-        # Generar fronteras absolutas del modelo tierra
         z_int = np.cumsum(np.concatenate(([0], thick))) - np.sum(thick)/2
         
         response = np.full_like(md, res[0], dtype=float)
         for i in range(len(z_int)-1):
             weight = 0.5 * (1 + np.tanh((tvd_rel - z_int[i]) / 5.0))
             response = response * (1 - weight) + res[i+1] * weight
-            
         return response * (1 + (ani - 1) * np.sin(alpha_rel)**2)
 
     def solve(self, mode, iters, obs, md, inc, dip, n_layers):
         bounds = [(0.5, 500)] * n_layers + [(5, 40)] * (n_layers - 1) + [(1, 3)]
-        x0 = [10]*n_layers + [15]*(n_layers-1) + [1.5]
+        res = differential_evolution(
+            lambda m: np.sqrt(np.mean((obs - self.forward_model(m, md, inc, dip, n_layers))**2)),
+            bounds=bounds, maxiter=iters, popsize=10
+        )
+        return res.x, res.fun
 
-        if mode == "Estocástico Global":
-            res = differential_evolution(
-                lambda m: np.sqrt(np.mean((obs - self.forward_model(m, md, inc, dip, n_layers))**2)),
-                bounds=bounds, maxiter=iters, popsize=12
-            )
-            return res.x, res.fun
-        else:
-            lb, ub = [b[0] for b in bounds], [b[1] for b in bounds]
-            res = least_squares(
-                lambda m: obs - self.forward_model(m, md, inc, dip, n_layers),
-                x0=x0, bounds=(lb, ub), max_nfev=iters
-            )
-            return res.x, np.mean(res.fun**2)
+    def predict_ahead(self, last_md, last_inc, user_dip, dist=100):
+        # Proyecta la trayectoria y la capa 100ft adelante
+        future_md = np.linspace(last_md, last_md + dist, 20)
+        # Trayectoria proyectada (asumiendo inc constante)
+        rel_path = (future_md - last_md) * np.sin(np.radians(last_inc - 90))
+        # Capas proyectadas (siguiendo el DIP)
+        rel_layer = -(future_md - last_md) * np.tan(np.radians(user_dip))
+        return future_md, rel_path, rel_layer
