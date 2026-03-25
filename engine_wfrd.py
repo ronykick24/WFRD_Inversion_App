9bb337ad6e1bf1c181cf5e61c1af6b4797807a41
ronykick24 <wert.pride@gmail.com>
2026-03-25 17:02:15 +0300

    Update engine_wfrd.py

--- engine_wfrd.py
+++ engine_wfrd.py
@@ -3,16 +3,19 @@ from scipy.optimize import differential_evolution
 from physics_engine import calculate_3d_horns
 
 def run_proactive_inversion(res_vals, inc_vals, layers):
-    """Encuentra el mejor Shift/DIP minimizando el error."""
+    # Limpiamos posibles NaNs antes de procesar
+    mask = ~np.isnan(res_vals) & ~np.isnan(inc_vals)
+    r_clean = res_vals[mask]
+    i_clean = inc_vals[mask]
+
     def objective(params):
         s_test, d_test = params
-        rh, rv = layers[2]['rh'], layers[2]['rv'] # Capa Reservorio
+        rh, rv = layers[2]['rh'], layers[2]['rv']
         
-        # Calculamos el error promedio en la ventana de datos
         errors = []
-        for i in range(len(res_vals)):
-            synth = calculate_3d_horns(rh, rv, inc_vals[i], d_test, s_test)
-            errors.append((np.log10(res_vals[i]) - np.log10(synth))**2)
+        for j in range(len(r_clean)):
+            synth = calculate_3d_horns(rh, rv, i_clean[j], d_test, s_test)
+            errors.append((np.log10(r_clean[j] + 1e-6) - np.log10(synth + 1e-6))**2)
         return np.mean(errors)
 
     result = differential_evolution(objective, bounds=[(-60, 60), (-15, 15)])
