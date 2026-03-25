302583d75ae5dbaffc2116094a1e947c861c2b89
ronykick24 <wert.pride@gmail.com>
2026-03-26 00:22:03 +0300

    Update engine_wfrd.py

--- engine_wfrd.py
+++ engine_wfrd.py
@@ -3,7 +3,7 @@ from scipy.optimize import differential_evolution
 from physics_engine import calculate_forward_model
 
 def run_ahta_inversion(res_data, inc_data, layers, iterations=1000):
-    """Inversión de 1000 iteraciones para resolver posición de capas."""
+    """Ajusta el modelo a los datos mediante evolución diferencial."""
     res_vals = np.array(res_data, dtype=float)
     inc_vals = np.array(inc_data, dtype=float)
     mask = ~np.isnan(res_vals)
@@ -11,10 +11,10 @@ def run_ahta_inversion(res_data, inc_data, layers, iterations=1000):
 
     def objective(params):
         shift_t, dip_t = params
-        rh, rv = layers[2]['rh'], layers[2]['rv'] # Target Layer
+        rh, rv = layers[2]['rh'], layers[2]['rv'] # Capa Objetivo
         errors = []
         for j in range(len(r_c)):
-            # Distancia proyectada punto a punto
+            # Distancia estructural punto a punto
             d_inst = shift_t + (j * np.tan(np.radians(dip_t)))
             synth = calculate_forward_model(rh, rv, i_c[j], dip_t, d_inst)
             errors.append((np.log10(r_c[j]+1e-6) - np.log10(synth+1e-6))**2)
