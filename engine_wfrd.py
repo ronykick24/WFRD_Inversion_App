870de767815d2d06d61e88bf6895f2f074ccaf21
ronykick24 <wert.pride@gmail.com>
2026-03-26 00:24:40 +0300

    Update engine_wfrd.py

--- engine_wfrd.py
+++ engine_wfrd.py
@@ -3,7 +3,7 @@ from scipy.optimize import differential_evolution
 from physics_engine import calculate_forward_model
 
 def run_ahta_inversion(res_data, inc_data, layers, iterations=1000):
-    """Ajusta el modelo a los datos mediante evolución diferencial."""
+    """Ajusta el modelo mediante evolución diferencial."""
     res_vals = np.array(res_data, dtype=float)
     inc_vals = np.array(inc_data, dtype=float)
     mask = ~np.isnan(res_vals)
@@ -11,14 +11,13 @@ def run_ahta_inversion(res_data, inc_data, layers, iterations=1000):
 
     def objective(params):
         shift_t, dip_t = params
-        rh, rv = layers[2]['rh'], layers[2]['rv'] # Capa Objetivo
+        rh, rv = layers[2]['rh'], layers[2]['rv'] 
         errors = []
         for j in range(len(r_c)):
-            # Distancia estructural punto a punto
             d_inst = shift_t + (j * np.tan(np.radians(dip_t)))
             synth = calculate_forward_model(rh, rv, i_c[j], dip_t, d_inst)
             errors.append((np.log10(r_c[j]+1e-6) - np.log10(synth+1e-6))**2)
         return np.mean(errors)
 
-    res = differential_evolution(objective, bounds=[(-65, 65), (-15, 15)], maxiter=iterations)
+    res = differential_evolution(objective, bounds=[(-65.0, 65.0), (-15.0, 15.0)], maxiter=iterations)
     return res.x
