6aa0646f8a9c75bcedab6d54962c5dbc6066cd5c
ronykick24 <wert.pride@gmail.com>
2026-03-25 16:57:34 +0300

    Update engine_wfrd.py

--- engine_wfrd.py
+++ engine_wfrd.py
@@ -2,15 +2,19 @@ import numpy as np
 from scipy.optimize import differential_evolution
 from physics_engine import calculate_3d_horns
 
-def run_proactive_inversion(measured_res, inc_val, layers):
-    """Algoritmo Estocástico: Encuentra el Shift y DIP óptimos."""
+def run_proactive_inversion(measured_res_array, md_array, inc_array, layers):
+    """Algoritmo Estocástico: Encuentra el Shift y DIP óptimos usando datos reales."""
     def objective(params):
         s_test, d_test = params
-        # Capa 2 es el TARGET RESERVOIR (100 ohmm)
-        rh, rv = layers[2]['rh'], layers[2]['rv']
-        synth = calculate_3d_horns(rh, rv, inc_val, d_test, s_test)
-        # Misfit logarítmico
-        return (np.log10(measured_res) - np.log10(synth))**2
+        rh, rv = layers[2]['rh'], layers[2]['rv'] # Capa Reservorio
+        
+        # Misfit promediado sobre los últimos puntos de datos
+        misfit_points = []
+        for i in range(len(measured_res_array)):
+            synth = calculate_3d_horns(rh, rv, inc_array[i], d_test, s_test)
+            misfit_points.append((np.log10(measured_res_array[i]) - np.log10(synth))**2)
+            
+        return np.mean(misfit_points)
 
     result = differential_evolution(objective, bounds=[(-60, 60), (-15, 15)])
     return result.x # Retorna [Best_Shift, Best_Dip]
