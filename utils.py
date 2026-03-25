c1b4acd2e8c01f1c7344327fe24db635a768a7b5
ronykick24 <wert.pride@gmail.com>
2026-03-25 17:14:15 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
-def get_palettes():
-    return {
-        "Earth/Tierra": [[0, "#4b2c20"], [0.5, "#8b4513"], [1, "#d2b48c"]],
-        "Reservoir/WFRD": [[0, "#00008B"], [0.3, "#00C8FF"], [0.7, "#FFFF00"], [1, "#FF8C00"]]
-    }
+def get_wfrd_palette():
+    """Paleta técnica para predecir contacto agua-aceite (OWC)."""
+    return [
+        [0.0, "#000044"],   # Agua Profunda
+        [0.3, "#00C8FF"],   # Acuífero
+        [0.6, "#FFFF00"],   # Oil Leg (Amarillo Brillante)
+        [1.0, "#4b2c20"]    # Sello / Shale
+    ]
