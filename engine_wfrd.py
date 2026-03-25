83fb4761b5ab76a5d961fbbe1c2789c115500160
ronykick24 <wert.pride@gmail.com>
2026-03-25 04:32:24 +0300

    Update engine_wfrd.py

--- engine_wfrd.py
+++ engine_wfrd.py
@@ -5,38 +5,40 @@ class WFRD_Engine_Core:
     def __init__(self):
         self.reach = 50.0
 
-    def forward_model(self, m, md, nbi_angle):
+    def forward_model(self, m, md, inc, dip):
         # m = [R1..R5, Esp1..Esp4, Ani]
         res = m[:5]
         thick = m[5:9]
         ani = m[9]
         
-        alpha_rel = np.radians(nbi_angle)
+        # Lógica Geofísica: NBI y Proyección de Capas
+        # Dip negativo -> Capas suben (+ TVD relativo)
+        # Dip positivo -> Capas bajan (- TVD relativo)
+        alpha_rel = np.radians(inc - dip)
         tvd_rel = md * np.sin(alpha_rel)
         
         z_int = np.cumsum(np.concatenate(([0], thick))) - np.sum(thick)/2
         
         response = np.full_like(md, res[0], dtype=float)
         for i in range(len(z_int)-1):
-            weight = 0.5 * (1 + np.tanh((tvd_rel - z_int[i]) / 5.5))
+            weight = 0.5 * (1 + np.tanh((tvd_rel - z_int[i]) / 5.0))
             response = response * (1 - weight) + res[i+1] * weight
             
         return response * (1 + (ani - 1) * np.sin(alpha_rel)**2)
 
     def solve(self, mode, obs, md, inc, dip, n_layers, iters):
-        nbi_val = inc - dip
         bounds = [(0.1, 500)]*n_layers + [(5, 25)]*(n_layers-1) + [(1, 4)]
         
         if mode == "Estocástico":
             res = differential_evolution(
-                lambda m: np.sqrt(np.mean((obs - self.forward_model(self.pad(m, n_layers), md, nbi_val))**2)),
-                bounds=bounds, maxiter=iters, popsize=8
+                lambda m: np.sqrt(np.mean((obs - self.forward_model(self.pad(m, n_layers), md, inc, dip))**2)),
+                bounds=bounds, maxiter=iters, popsize=10
             )
             return self.pad(res.x, n_layers), res.fun
         else:
             x0 = [10]*n_layers + [12]*(n_layers-1) + [1.5]
             res = least_squares(
-                lambda m: obs - self.forward_model(self.pad(m, n_layers), md, nbi_val),
+                lambda m: obs - self.forward_model(self.pad(m, n_layers), md, inc, dip),
                 x0=x0, bounds=([b[0] for b in bounds], [b[1] for b in bounds]), max_nfev=iters
             )
             return self.pad(res.x, n_layers), np.mean(res.fun**2)
