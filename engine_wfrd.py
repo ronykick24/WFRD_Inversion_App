9b1cfd2b853108de4a35529564fb52d5e3af27e3
ronykick24 <wert.pride@gmail.com>
2026-03-26 00:28:08 +0300

    Update engine_wfrd.py

--- engine_wfrd.py
+++ engine_wfrd.py
@@ -3,7 +3,6 @@ from scipy.optimize import differential_evolution
 from physics_engine import calculate_forward_model
 
 def run_ahta_inversion(res_data, inc_data, layers, iterations=1000):
-    """Inversión estocástica para resolver DIP y DTBss."""
     res_vals = np.array(res_data, dtype=float)
     inc_vals = np.array(inc_data, dtype=float)
     mask = ~np.isnan(res_vals)
@@ -12,12 +11,8 @@ def run_ahta_inversion(res_data, inc_data, layers, iterations=1000):
     def objective(params):
         shift_t, dip_t = params
         rh, rv = layers[2]['rh'], layers[2]['rv'] 
-        errors = []
-        for j in range(len(r_c)):
-            d_inst = shift_t + (j * np.tan(np.radians(dip_t)))
-            synth = calculate_forward_model(rh, rv, i_c[j], dip_t, d_inst)
-            errors.append((np.log10(r_c[j]+1e-6) - np.log10(synth+1e-6))**2)
+        errors = [ (np.log10(r_c[j]+1e-6) - np.log10(calculate_forward_model(rh, rv, i_c[j], dip_t, shift_t + (j * np.tan(np.radians(dip_t))))+1e-6))**2 for j in range(len(r_c)) ]
         return np.mean(errors)
 
     res = differential_evolution(objective, bounds=[(-65.0, 65.0), (-15.0, 15.0)], maxiter=iterations)
-    return [float(x) for x in res.x] # Retornar como lista de floats nativos
+    return [float(x) for x in res.x]
