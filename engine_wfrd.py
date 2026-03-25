8144057f6118d1166790cad1637bdc7b9a9695aa
ronykick24 <wert.pride@gmail.com>
2026-03-25 22:33:44 +0300

    Update engine_wfrd.py

--- engine_wfrd.py
+++ engine_wfrd.py
@@ -3,7 +3,7 @@ from scipy.optimize import differential_evolution
 from physics_engine import calculate_forward_model
 
 def run_ahta_inversion(res_data, inc_data, layers, iterations=1000):
-    """Encuentra el mejor DIP y DTBss minimizando el error logarítmico."""
+    """Inversión de 1000 iteraciones para resolver posición de capas."""
     res_vals = np.array(res_data, dtype=float)
     inc_vals = np.array(inc_data, dtype=float)
     mask = ~np.isnan(res_vals)
@@ -11,18 +11,14 @@ def run_ahta_inversion(res_data, inc_data, layers, iterations=1000):
 
     def objective(params):
         shift_t, dip_t = params
-        # Capa objetivo (Reservorio) definida en el índice 2 de layers
-        rh, rv = layers[2]['rh'], layers[2]['rv']
-        
+        rh, rv = layers[2]['rh'], layers[2]['rv'] # Target Layer
         errors = []
         for j in range(len(r_c)):
-            # Distancia estructural calculada para cada punto de la muestra
+            # Distancia proyectada punto a punto
             d_inst = shift_t + (j * np.tan(np.radians(dip_t)))
             synth = calculate_forward_model(rh, rv, i_c[j], dip_t, d_inst)
-            # Misfit logarítmico para manejar rangos de resistividad
             errors.append((np.log10(r_c[j]+1e-6) - np.log10(synth+1e-6))**2)
         return np.mean(errors)
 
-    # Optimización estocástica global
     res = differential_evolution(objective, bounds=[(-65, 65), (-15, 15)], maxiter=iterations)
-    return res.x # [Shift, Dip]
+    return res.x
