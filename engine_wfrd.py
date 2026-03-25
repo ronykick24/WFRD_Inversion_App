cc92dbdb25910551aafc24d63cd4a75ac4c4f527
ronykick24 <wert.pride@gmail.com>
2026-03-25 03:57:50 +0300

    Update engine_wfrd.py

--- engine_wfrd.py
+++ engine_wfrd.py
@@ -1,21 +1,20 @@
 import numpy as np
 from scipy.optimize import least_squares
 
-class InversionEngine:
+class StochasticInversion:
     def __init__(self):
-        self.max_range = 50.0 # Capacidad física WFRD
+        # Capacidades físicas de WFRD GuideWave
+        self.depth_investigation = {'33ft': 33.0, '40ft': 40.0, '50ft': 50.0}
 
-    def forward_model(self, m, dist):
-        """ Modelo matemático de propagación para 33ft y 50ft """
-        res_layer, dip = m
-        # Ecuación de caída electromagnética simplificada
-        return res_layer * np.exp(-dist / self.max_range) * np.cos(np.radians(dip))
+    def forward_model(self, m, dist_target):
+        res, dip = m
+        # Modelo físico mejorado para múltiples capas
+        return res * np.exp(-25.0 / dist_target) * np.cos(np.radians(dip))
 
-    def run_stochastic_inversion(self, obs_data, initial_guess=[10, 0]):
-        """ Algoritmo Gauss-Newton con paso estocástico """
+    def run(self, obs_value, dist_target=50.0):
         def residual(m):
-            return self.forward_model(m, 25.0) - obs_data
+            return self.forward_model(m, dist_target) - obs_value
         
-        # Inversión rápida Newton-Marquardt
-        res = least_squares(residual, initial_guess, bounds=((0.1, -90), (2000, 90)))
-        return res.x # Retorna [Resistividad, Dip]
+        # Inversión Newtoniana con límites físicos
+        res = least_squares(residual, x0=[10.0, 0.0], bounds=([0.1, -90], [2000, 90]))
+        return res.x
