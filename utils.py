20d9708f3fae82e30bdd3707536361c2eafcad73
ronykick24 <wert.pride@gmail.com>
2026-03-25 16:57:13 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,11 +2,7 @@ import pandas as pd
 
 def get_owc_palette():
     """Paleta de Alto Relieve para Contacto Agua-Aceite."""
-    return [
-        [0.0, "#000032"], [0.3, "#00C8FF"], # Agua
-        [0.6, "#FFD700"], [1.0, "#4b2c20"]  # Aceite / Sello
-    ]
+    return [[0.0, "#000032"], [0.3, "#00C8FF"], [0.6, "#FFD700"], [1.0, "#4b2c20"]]
 
 def save_geosteering_report(data):
-    """Convierte diccionario de datos en DataFrame."""
     return pd.DataFrame([data])
