78605373e08f56e2a8d1067e444c4e38fbfce5c9
ronykick24 <wert.pride@gmail.com>
2026-03-25 04:48:36 +0300

    Update engine_wfrd.py

--- engine_wfrd.py
+++ engine_wfrd.py
@@ -3,16 +3,19 @@ from scipy.optimize import differential_evolution
 
 class WFRD_Engine_Core:
     def __init__(self):
-        self.reach = 50.0
+        pass
 
-    def forward_model(self, m, md, inc, dip):
-        # m = [R1..R5, E1..E4, Ani]
-        res, thick, ani = m[:5], m[5:9], m[9]
+    def forward_model(self, m, md, inc, dip, n_layers):
+        # m estructurado: [Resistividades, Espesores, Anisotropía]
+        res = m[:n_layers]
+        thick = m[n_layers:2*n_layers-1]
+        ani = m[-1]
+        
         alpha_rel = np.radians(inc - dip)
         tvd_rel = md * np.sin(alpha_rel)
         
-        # Interfaces relativas al centro del reservorio (Capa 4)
-        z_int = np.cumsum(np.concatenate(([0], thick))) - np.sum(thick[:3]) 
+        # Interfaces centradas en la capa objetivo (capa central)
+        z_int = np.cumsum(np.concatenate(([0], thick))) - np.sum(thick)/2
         
         response = np.full_like(md, res[0], dtype=float)
         for i in range(len(z_int)-1):
@@ -21,11 +24,17 @@ class WFRD_Engine_Core:
             
         return response * (1 + (ani - 1) * np.sin(alpha_rel)**2)
 
-    def solve(self, mode, iters, obs, md, inc, dip):
-        # Secuencia: Sello1, Arena1, Sello2, Reservorio(PAY), Sello3
-        bounds = [(1, 5), (10, 40), (1, 5), (40, 500), (1, 5)] + [(10, 30)]*4 + [(1, 2.5)]
+    def solve(self, iters, obs, md, inc, dip, n_layers):
+        # Definición de límites según litología (Sello -> Reservorio -> Sello)
+        res_bounds = [(0.5, 10)] * n_layers
+        # Forzamos que la capa central sea la más resistiva (Reservorio)
+        res_bounds[n_layers // 2] = (50, 500) 
+        
+        thick_bounds = [(5, 30)] * (n_layers - 1)
+        bounds = res_bounds + thick_bounds + [(1, 3)]
+        
         res = differential_evolution(
-            lambda m: np.sqrt(np.mean((obs - self.forward_model(m, md, inc, dip))**2)),
-            bounds=bounds, maxiter=iters, popsize=8
+            lambda m: np.sqrt(np.mean((obs - self.forward_model(m, md, inc, dip, n_layers))**2)),
+            bounds=bounds, maxiter=iters, popsize=10
         )
         return res.x, res.fun
