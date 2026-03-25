2ee223aedb224ed2446af984a9a3c249f3c11cca
ronykick24 <wert.pride@gmail.com>
2026-03-25 17:00:36 +0300

    Update engine_wfrd.py

--- engine_wfrd.py
+++ engine_wfrd.py
@@ -2,18 +2,18 @@ import numpy as np
 from scipy.optimize import differential_evolution
 from physics_engine import calculate_3d_horns
 
-def run_proactive_inversion(measured_res_array, md_array, inc_array, layers):
-    """Algoritmo Estocástico: Encuentra el Shift y DIP óptimos usando datos reales."""
+def run_proactive_inversion(res_vals, inc_vals, layers):
+    """Encuentra el mejor Shift/DIP minimizando el error."""
     def objective(params):
         s_test, d_test = params
         rh, rv = layers[2]['rh'], layers[2]['rv'] # Capa Reservorio
         
-        misfit_points = []
-        for i in range(len(measured_res_array)):
-            synth = calculate_3d_horns(rh, rv, inc_array[i], d_test, s_test)
-            misfit_points.append((np.log10(measured_res_array[i]) - np.log10(synth))**2)
-            
-        return np.mean(misfit_points)
+        # Calculamos el error promedio en la ventana de datos
+        errors = []
+        for i in range(len(res_vals)):
+            synth = calculate_3d_horns(rh, rv, inc_vals[i], d_test, s_test)
+            errors.append((np.log10(res_vals[i]) - np.log10(synth))**2)
+        return np.mean(errors)
 
     result = differential_evolution(objective, bounds=[(-60, 60), (-15, 15)])
     return result.x
