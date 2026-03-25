bf1a5627bd9ca2829f031cd40ec6f9b8e930d4b9
ronykick24 <wert.pride@gmail.com>
2026-03-25 04:34:59 +0300

    Update engine_wfrd.py

--- engine_wfrd.py
+++ engine_wfrd.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.optimize import differential_evolution, least_squares
+from scipy.optimize import differential_evolution
 
 class WFRD_Engine_Core:
     def __init__(self):
@@ -7,13 +7,9 @@ class WFRD_Engine_Core:
 
     def forward_model(self, m, md, inc, dip):
         # m = [R1..R5, Esp1..Esp4, Ani]
-        res = m[:5]
-        thick = m[5:9]
-        ani = m[9]
+        res, thick, ani = m[:5], m[5:9], m[9]
         
-        # Lógica Geofísica: NBI y Proyección de Capas
-        # Dip negativo -> Capas suben (+ TVD relativo)
-        # Dip positivo -> Capas bajan (- TVD relativo)
+        # Geometría: Dip (-) Ascendente, Dip (+) Descendente
         alpha_rel = np.radians(inc - dip)
         tvd_rel = md * np.sin(alpha_rel)
         
@@ -26,22 +22,13 @@ class WFRD_Engine_Core:
             
         return response * (1 + (ani - 1) * np.sin(alpha_rel)**2)
 
-    def solve(self, mode, obs, md, inc, dip, n_layers, iters):
-        bounds = [(0.1, 500)]*n_layers + [(5, 25)]*(n_layers-1) + [(1, 4)]
-        
-        if mode == "Estocástico":
-            res = differential_evolution(
-                lambda m: np.sqrt(np.mean((obs - self.forward_model(self.pad(m, n_layers), md, inc, dip))**2)),
-                bounds=bounds, maxiter=iters, popsize=10
-            )
-            return self.pad(res.x, n_layers), res.fun
-        else:
-            x0 = [10]*n_layers + [12]*(n_layers-1) + [1.5]
-            res = least_squares(
-                lambda m: obs - self.forward_model(self.pad(m, n_layers), md, inc, dip),
-                x0=x0, bounds=([b[0] for b in bounds], [b[1] for b in bounds]), max_nfev=iters
-            )
-            return self.pad(res.x, n_layers), np.mean(res.fun**2)
+    def solve(self, obs, md, inc, dip, n_layers):
+        bounds = [(0.1, 500)]*n_layers + [(5, 30)]*(n_layers-1) + [(1, 4)]
+        res = differential_evolution(
+            lambda m: np.sqrt(np.mean((obs - self.forward_model(self.pad(m, n_layers), md, inc, dip))**2)),
+            bounds=bounds, popsize=10, maxiter=50
+        )
+        return self.pad(res.x, n_layers), res.fun
 
     def pad(self, m, n):
         res, thick = np.zeros(5), np.zeros(4)
