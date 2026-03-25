adc2230a39898ab95678ea68c98684148f19dc71
ronykick24 <wert.pride@gmail.com>
2026-03-25 17:00:51 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,5 @@
 import pandas as pd
 
-def get_owc_palette():
-    """Paleta de Alto Relieve para Contacto Agua-Aceite."""
-    return [[0.0, "#000032"], [0.3, "#00C8FF"], [0.6, "#FFD700"], [1.0, "#4b2c20"]]
-
 def save_geosteering_report(data):
+    """Crea un reporte rápido en formato DataFrame."""
     return pd.DataFrame([data])
